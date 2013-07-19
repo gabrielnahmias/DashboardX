0d531755852ae16cae7a8edf5f1b086022eb0af6
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-19 14:59:05 -0500

    Removed namespace "DashboardX" and static declaration from Globals.

--- DashboardX/Classes/Globals.cs
+++ DashboardX/Classes/Globals.cs
@@ -4,21 +4,18 @@ using System.Data;
 using System.Linq;
 using System.Web;
 
-namespace DashboardX
+public class Globals
 {
-    public static class Globals
+    public class Dirs
     {
-        public class Dirs
-        {
-            public static string Assets = "assets",
-                                 CSS = Assets + "/css",
-                                 Images = Assets + "/img",
-                                 Icons = Images + "/icons",
-                                 JS = Assets + "/js";
-        }
-        public static string GridName = "MainGrid";
-        // In case the SQL connection is lost, use this for a data source
-        // TODO: Set up local SQL Server.
-        //public static DataSet Data = Excel.GetDataSetFromFile(@"C:\Projects\Dashboard\DashboardX\etc\sampledata.xls");
+        public static string Assets = "assets",
+                                CSS = Assets + "/css",
+                                Images = Assets + "/img",
+                                Icons = Images + "/icons",
+                                JS = Assets + "/js";
     }
+    public static string GridName = "MainGrid";
+    // In case the SQL connection is lost, use this for a data source
+    // TODO: Set up local SQL Server.
+    //public static DataSet Data = Excel.GetDataSetFromFile(@"C:\Projects\Dashboard\DashboardX\etc\sampledata.xls");
 }
