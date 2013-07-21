eb6f91aa0bd9198f5872c5247b1494366bc6c9dc
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-21 16:43:20 -0500

    Added for exporting charts to images.

---
+++ DashboardX/Classes/HtmlChartExportSettings.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+/// <summary>
+/// The settings needed for exporting the RadHtmlChart
+/// </summary>
+public class HtmlChartExportSettings
+{
+	private int _Height;
+	private int _Width;
+	private string _SvgFilePath;
+	private string _OutputFilePath;
+	private string _Extension;
+	private string _ClientFileName;
+
+	public HtmlChartExportSettings()
+	{
+        
+	}
+
+	/// <summary>
+	/// The path to the Inkscape executable on the server.
+	/// </summary>
+    public const string InkscapePath = @"C:\Projects\Dashboard\DashboardX\etc\inkscape\InkscapePortable.exe";//the path to the Inkscape executable on your machine
+
+	/// <summary>
+	/// A list with the appropriate content-type values for the file formats.
+	/// </summary>
+	public static readonly Dictionary<string, string> ContentTypeList = new Dictionary<string, string>
+		{
+			{"png", "image/png"},
+			{"pdf", "application/pdf"}
+		};
+
+	/// <summary>
+	/// The height of the exported file, usually the height of the chart.
+	/// </summary>
+	public int Height
+	{
+		get { return _Height; }
+		set { _Height = value; }
+	}
+
+	/// <summary>
+	/// The width of the exported file, usually the width of the chart.
+	/// </summary>
+	public int Width
+	{
+		get { return _Width; }
+		set { _Width = value; }
+	}
+
+	/// <summary>
+	/// The path to the SVG file that will be passed to Inkscape.
+	/// </summary>
+	public string SvgFilePath
+	{
+		get { return _SvgFilePath; }
+		set { _SvgFilePath = value; }
+	}
+
+	/// <summary>
+	/// The paht to the file created by Inkscape that will be sent to the client.
+	/// </summary>
+	public string OutputFilePath
+	{
+		get { return _OutputFilePath; }
+		set { _OutputFilePath = value; }
+	}
+
+	/// <summary>
+	/// The format in which the chart will be exported.
+	/// </summary>
+	public string Extension
+	{
+		get { return _Extension; }
+		set { _Extension = value; }
+	}
+
+	/// <summary>
+	/// The file name the client will receive for the file.
+	/// </summary>
+	public string ClientFileName
+	{
+		get { return _ClientFileName; }
+		set { _ClientFileName = value +"." + this.Extension; }
+	}
+}
---
+++ DashboardX/Classes/HtmlChartExporter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.IO;
+
+/// <summary>
+/// Provides the core functionality for exporing the RadHtmlChart - SVG conversion and a helper method for sending the file to the client.
+/// </summary>
+public static class HtmlChartExporter
+{
+	/// <summary>
+	/// Uses a third party library called Inkscape to create a file from the SVG representation of the RadHtmlChart (already saved as a file).
+	/// </summary>
+	/// <param name="settings">The current settings that are used on the page.</param>
+	public static void ExportHtmlChart(HtmlChartExportSettings settings)
+	{
+		// The actual export takes place here
+		// Full list of export options is available at
+		// http://tavmjong.free.fr/INKSCAPE/MANUAL/html/CommandLine-Export.html
+		Process inkscape = new Process();
+        string sArgs = String.Format("--file \"{0}\" --export-{1} \"{2}\"" + ( (settings.Width != null && settings.Height!=null && settings.Width!=0 && settings.Height!=0) ? " --export-width {3} --export-height {4}" : "" ),
+                                        settings.SvgFilePath, settings.Extension, settings.OutputFilePath, settings.Width, settings.Height);
+		inkscape.StartInfo.FileName = HtmlChartExportSettings.InkscapePath;
+        Debug.WriteLine("Inkscape Args: " + sArgs);
+        inkscape.StartInfo.Arguments = sArgs;
+		inkscape.StartInfo.UseShellExecute = true;
+        try
+        {
+            inkscape.Start();
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Inkscape path is incorrect: " + HtmlChartExportSettings.InkscapePath);
+        }
+		inkscape.WaitForExit();
+	}
+
+	/// <summary>
+	/// Returns a byte[] array from the file so that it can be sent to the browser.
+	/// </summary>
+	/// <param name="filePath">the physical path to the file to read.</param>
+	/// <returns>Returns a byte[] array</returns>
+	public static byte[] ReadFile(string filePath)
+	{
+		byte[] buffer;
+		FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+		try
+		{
+			int length = (int)fileStream.Length;  // get file length
+			buffer = new byte[length];            // create buffer
+			int count;                            // actual number of bytes read
+			int sum = 0;                          // total number of bytes read
+
+			// read until Read method returns 0 (end of the stream has been reached)
+			while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+				sum += count;  // sum is a buffer offset for next reading
+		}
+		finally
+		{
+			fileStream.Close();
+		}
+		return buffer;
+	}
+}
