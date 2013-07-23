0a4b521b8a5d66662916cbe38d7baeb71ed19b4c
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-23 17:28:40 -0500

    Changed grid name (unused?).

--- DashboardX/Classes/Globals.cs
+++ DashboardX/Classes/Globals.cs
@@ -14,7 +14,7 @@ public class Globals
                                 Icons = Images + "/icons",
                                 JS = Assets + "/js";
     }
-    public static string GridName = "MainGrid";
+    public static string GridName = "RadGrid1";
     // In case the SQL connection is lost, use this for a data source
     // TODO: Set up local SQL Server.
     //public static DataSet Data = Excel.GetDataSetFromFile(@"C:\Projects\Dashboard\DashboardX\etc\sampledata.xls");
