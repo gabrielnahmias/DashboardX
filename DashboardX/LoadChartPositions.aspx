d57a9f6c8e29d1b472935defa124b8dd5ae0a60e
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-26 17:57:55 -0500

    Loads chart positions into a JSON object.

---
+++ DashboardX/LoadChartPositions.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoadChartPositions.aspx.cs" Inherits="DashboardX.LoadChartPositions" %>
---
+++ DashboardX/LoadChartPositions.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DashboardX
+{
+    public partial class LoadChartPositions : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Response.AddHeader("Content-type", "text/json");
+            Response.AddHeader("Content-type", "application/json");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i = 1;
+            
+            string sql = "select * from dbx.dbo.ChartPositions";
+
+            SqlComm conn = new SqlComm();
+
+            //Response.Write(String.Format("SQL Query: {0}\n", sql));
+
+            //Response.Write("Result:\n");
+            
+            // Load chart positions.
+            conn.getResult(sql);
+
+            Response.Write("{");
+
+            foreach (object[] o in conn.Result)
+            {
+                string sKey = o[0].ToString();
+                int iPlace = int.Parse(o[1].ToString());
+                Response.Write(String.Format("\"{0}\": {1}{2}", sKey, iPlace, ((i < conn.Result.Count) ? "," : "") ));
+                i++;
+            }
+
+            Response.Write("}");
+        }
+    }
+}
