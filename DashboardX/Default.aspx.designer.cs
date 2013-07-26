74db125c7dbd941933dedf6d5aaa27a6f7350b68
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-26 14:45:43 -0500

    Updated.

--- DashboardX/Default.aspx.designer.cs
+++ DashboardX/Default.aspx.designer.cs
@@ -30,6 +30,15 @@ public partial class Default {
     protected global::System.Web.UI.HtmlControls.HtmlForm main;
     
     /// <summary>
+    /// QsfFormDecorator control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadFormDecorator QsfFormDecorator;
+    
+    /// <summary>
     /// RadScriptManager control.
     /// </summary>
     /// <remarks>
