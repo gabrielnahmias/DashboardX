023ff85568b3b2e8c623c8cf258d5905e15f13e9
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-07-26 17:59:36 -0500

    Finalized functionality.

--- DashboardX/UpdateChartPosition.aspx.cs
+++ DashboardX/UpdateChartPosition.aspx.cs
@@ -13,18 +13,25 @@ namespace DashboardX
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Chart cp = new Chart();
             HttpContext c = HttpContext.Current;
             JavaScriptSerializer js = new JavaScriptSerializer();
-            Chart cp = new Chart();
+            SqlComm conn = new SqlComm();
 
             if (c.Request["change"] != null)
                 cp = js.Deserialize<Chart>(c.Request["change"]);
-            
+
+            string sql = String.Format("declare @oldchartname varchar(50), @movedchartoldpos int;" +
+                                       "set @oldchartname = (select Type from dbx.dbo.ChartPositions where Position={0});" +
+                                       "set @movedchartoldpos = (select Position from dbx.dbo.ChartPositions where Type='{1}');" +
+                                       "update dbx.dbo.ChartPositions set Position={0} where Type='{1}';" +
+                                       "update dbx.dbo.ChartPositions set Position=@movedchartoldpos where Type=@oldchartname;", cp.Position, cp.Type);
+
+            // Switch chart positions.
+            conn.Execute(sql);
+
             Response.Write(cp.Type[0].ToString().ToUpper() + cp.Type.Substring(1) + " chart has been changed to position " + cp.Position + ".");
-
-            SqlComm conn = new SqlComm();
-
-            //conn.getResult("update dbx.dbo.ChartPosition set ");
+            Response.Write("\nSQL Query: " + sql);
         }
     }
 }
