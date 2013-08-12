1ef6d94ee898276047f2385513de87ea2e96e5e7
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-08-12 14:01:53 -0500

    Updated.

--- DashboardX/Default.aspx.designer.cs
+++ DashboardX/Default.aspx.designer.cs
@@ -21,6 +21,15 @@ public partial class Default {
     protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource_Stores;
     
     /// <summary>
+    /// SqlDataSource_SelectedStore control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource_SelectedStore;
+    
+    /// <summary>
     /// RadStyleSheetManager control.
     /// </summary>
     /// <remarks>
