c7efef5be7cefef900160dd09cad9d2d43ca96c1
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-08-15 14:17:10 -0500

    Removed commented from Page_Init() (leaving it empty for now) and reworked SQL (new query that grabs different colors).

--- DashboardX/pages/Charts.ascx.cs
+++ DashboardX/pages/Charts.ascx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -15,25 +16,22 @@ namespace DashboardX.pages
     {
         protected void Page_Init(object sender, EventArgs e)
         {
-            //Response.Write("test");
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            string sql = "";
-
             HttpContext c = HttpContext.Current;
+            string sql = "",
+                   sLID = null;
 
-            if (c.Request["store"] != null)
-            {
-                // Store specified so use that in query (DBAName).
-                sql = String.Format("select SUM(TransAmount) as TotalSales, Color, SubmitDate from dbx.dbo.SampleData where DBAName='{0}' group by Color, SubmitDate", c.Request["store"]);
-            }
+            if (c.Request["lid"] != null)
+                sLID = c.Request["lid"].ToString();
+
+            // If a store is specified, use it in the query (LocationID), otherwise use default query.
+            if (sLID != null)
+                sql = String.Format("with T as (select (row_number() over (order by SubmitDate)) as RowNo, sum(TransAmount) as TotalSales, SubmitDate, Color from dbx.dbo.SampleData where LocationID={0} group by Color, SubmitDate ) select * from ( select SubmitDate, TotalSales, case when RowNo=1 or RowNo=6 or RowNo=11 then 'Red' when RowNo=2 or RowNo=7 or RowNo=12 then 'Orange' when RowNo=3 or RowNo=8 or RowNo=13 then 'Yellow' when RowNo=4 or RowNo=9 or RowNo=14 then 'Green' when RowNo=5 or RowNo=10 or RowNo=15 then 'Blue' end as Color from T ) x group by SubmitDate, Color, TotalSales", sLID);
             else
-            {
-                // No store specified so use default query.
-                sql = "select SUM(TransAmount) as TotalSales, Color, SubmitDate from dbx.dbo.SampleData group by Color, SubmitDate";
-            }
+                sql = "select sum(TransAmount) as TotalSales, Color, SubmitDate from dbx.dbo.SampleData group by Color, SubmitDate";
 
             // Assign to proper SelectCommand.
             SqlDataSource_TotalSales.SelectCommand = sql;
