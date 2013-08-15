8d9e53cdd809aeef9107528ec1dc1f3593246b7a
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-08-15 14:13:20 -0500

    Moved console-related code to Console.js, added override settings for Console.js, and changed DBX.console to Console.

--- DashboardX/assets/js/scripts.js
+++ DashboardX/assets/js/scripts.js
@@ -1,65 +1,11 @@
 // Only global namespace.
 var DBX = {
-    console: {
-        //Settings
-        formatting: {
-            debug: {
-                showInfo: true
-            },
-            stackTrace: {
-                ignoreDebugFuncs: true,
-                spacing: false
-            }
-        }
-    },
     events: {},
-    utils: {},
-
-    // Settings
-    debugging: true,
-    trace: false//true
+    utils: {}
 };
 
-// Commonly used functions
-DBX.console.debug = function () {
-    var args = ((typeof arguments !== 'undefined') ? Array.prototype.slice.call(arguments, 0) : []),
-        aLines = DBX.console.stackTrace().split("\n"),
-        aCurrentLine = aLines[2].split(/[@:]/),
-        sBorder = "--------------------------------------------------------------------------------------------",
-        sLines = "";
+Console.settings.stackTrace.enabled = false;
 
-    // Show info if the setting is true and there's no extra trace (would be kind of pointless).
-    if (DBX.console.formatting.debug.showInfo && !DBX.trace) {
-        var sFunc = aCurrentLine[0],
-            sURL = aCurrentLine[1],
-            sLine = aCurrentLine[2];
-        console.debug("On l
-    }
-
-    // If the setting permits, get rid of the two obvious debug functions (DBX.console.debug and DBX.console.stackTrace).
-    if (DBX.console.formatting.stackTrace.ignoreDebugFuncs) {
-        aLines.shift();
-        aLines.shift();
-    }
-
-    sLines = aLines.join(((DBX.console.formatting.stackTrace.spacing) ? "\n\n" : "\n")).trim();
-
-    trace = typeof trace !== 'undefined' ? trace : true;
-    if (typeof console != "undefined" && DBX.debugging == true) {
-        for (var arg in args)
-            console.debug(args[arg]);
-
-        if (DBX.trace) {
-            console.debug(sBorder + "\nStack Trace\n" + sBorder + "\n");
-            console.debug(sLines);
-            console.debug(sBorder + "\n\n");
-        }
-    }
-}
-DBX.console.stackTrace = function () {
-    var err = new Error();
-    return err.stack;
-}
 DBX.utils.getParameterByName = function(name) {
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -67,7 +13,7 @@ DBX.utils.getParameterByName = function(name) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 DBX.utils.disableSelection = function(target) {
-    DBX.console.debug(target);
+    Console.debug(target);
 
     if (typeof target.onselectstart != "undefined")             // For IE
         target.onselectstart = function() { return false };
@@ -143,7 +89,7 @@ $(function() {
             inside = false;
 
         $(window).scroll(function () {
-            //DBX.console.debug('scrolling');
+            //Console.debug('scrolling');
             position = $(window).scrollTop();
             if (position >= topDistance && !inside) {
                 fadeHeader();
