c2185a6128abddd290cd4fd9b07a745eb9879284
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2013-09-08 15:03:53 -0500

    Changed InitChart to Settings.

--- DashboardX/Settings.aspx.cs
+++ DashboardX/Settings.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace DashboardX
 {
-    public partial class InitChart : System.Web.UI.Page
+    public partial class Settings : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
